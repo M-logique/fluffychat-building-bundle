974b616c68f49ae62bd01c6fae0c7ad45e3231ff
M-logique <logique@duck.com>
2026-04-28 00:15:22 +0330

    Update goon.sh

--- goon.sh
+++ goon.sh
@@ -3,7 +3,7 @@
 URL="https://ftp.fau.de/kiwix/zim/wikipedia/wikipedia_en_all_maxi_2026-02.zim"
 BASE="wikipedia_en_all_maxi_2026-02.zim.part"
 
-curl -L "$URL" | split -b 50M -d - "$BASE-" \
+curl -L "$URL" | split -a 4 -b 50M -d - "$BASE-" \
 --filter='
     echo "Uploading $FILE"
     curl --retry 5 --retry-delay 5 -T - "https://matrix.hipoo.ir/fat/$FILE"
