994fbe9552a64f2493c2f8b4c8fa3ab908c6fe9c
M. logique <100792944+M-logique@users.noreply.github.com>
2026-04-28 10:02:55 +0330

    Update goon.sh

--- goon.sh
+++ goon.sh
@@ -1,10 +1,22 @@
-# !/usr/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 URL="https://ftp.fau.de/kiwix/zim/wikipedia/wikipedia_en_all_maxi_2026-02.zim"
 BASE="wikipedia_en_all_maxi_2026-02.zim.part"
 
 curl -L "$URL" | split -a 4 -b 50M -d - "$BASE-" \
 --filter='
-    echo "Uploading $FILE"
+  
+  fname="$(basename "$FILE")"
+  part="${fname##*-}"           
+  part_num="$((10#$part))"      
+
+  if [ "$part_num" -gt 311 ]; then
+    echo "Uploading $FILE (part $part_num > 310)"
     curl --retry 5 --retry-delay 5 -T - "https://jchat.space/fat/fuck/$FILE"
+  else
+    echo "Skipping upload for $FILE (part $part_num <= 311)"
+    
+    cat >/dev/null
+  fi
 '
